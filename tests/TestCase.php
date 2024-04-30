780781a298536bfb41ad4e36df1f725e87892de1
Mark <14284867+xHeaven@users.noreply.github.com>
2024-05-01 01:16:38 +0200

    make tests work

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,13 +3,24 @@
 namespace Luminarix\JSONL\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Luminarix\JSONL\JSONLServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
-class TestCase
+class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
+        parent::setUp();
+
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Luminarix\\JSONL\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            JSONLServiceProvider::class,
+        ];
     }
 }
